8020c968ecfd659a02bec69827152a3a11c8beaa
Mifra Burneo <mifraburneo@gmail.com>
2022-10-09 22:35:49 +0200

    Refactor

--- installation/navi_recompilation.sh
+++ installation/navi_recompilation.sh
@@ -1,5 +1,38 @@
 #/bin/bash
 
+OS=()
+help() {
+  cat << EOF
+  Usage: $0 -linux -macos -windows
+EOF
+}
+
+if [ $# -eq 0 ]; then
+  help
+  exit 1
+fi
+
+# Get mandatory arguments -linux, -macos, -windows and help optional
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -linux)
+      OS+=("linux") ;;
+    -macos)
+      OS+=("macos") ;;
+    -windows)
+      OS+=("windows") ;;
+    -help)
+      help
+      exit 0 ;;
+    *)
+      echo "Invalid option: $1"
+      help
+      exit 1 ;;
+  esac
+  shift
+done
+
+
 git clone https://github.com/denisidoro/navi navi_repo
 
 echo "" > navi_repo/docs/navi.cheat 
@@ -33,44 +66,58 @@ fi
 
 cd navi_repo
 
-# Add targets
-rustup target add x86_64-unknown-linux-gnu
-rustup target add x86_64-apple-darwin
-rustup target add x86_64-pc-windows-gnu
-
-# Compile code for Linux
-cargo build --target="x86_64-unknown-linux-gnu"
-if [ $? -eq 0 ]; then
-    echo "Compilation for Linux successful!"
-else
-    echo "Compilation for Linux failed!"
-    exit 10
-fi
-
-# Compile code for MacOS
-cargo build --target="x86_64-apple-darwin"
-if [ $? -eq 0 ]; then
-    echo "Compilation for MacOS successful!"
-else
-    echo "Compilation for MacOS failed!"
-    exit 11
-fi
-# Compile code for Windows
-cargo build --target="x86_64-pc-windows-gnu"
-if [ $? -eq 0 ]; then
-    echo "Compilation for Windows successful!"
-else
-    echo "Compilation for Windows failed!"
-    exit 12
-fi
+for os in "${OS[@]}"; do
+    echo "Building for $os..."
+    if [ "$os" == "linux" ]; then
+        rustup target add x86_64-unknown-linux-musl
+        # Compile code for Linux
+        cargo build --target="x86_64-unknown-linux-musl"
+        if [ $? -eq 0 ]; then
+            echo "Compilation for Linux successful!"
+        else
+            echo "Compilation for Linux failed!"
+            exit 10
+        fi
+    elif [ "$os" == "macos" ]; then
+        rustup target add x86_64-apple-darwin
+        # Compile code for MacOS
+        cargo build --target="x86_64-apple-darwin"
+        if [ $? -eq 0 ]; then
+            echo "Compilation for MacOS successful!"
+        else
+            echo "Compilation for MacOS failed!"
+            exit 11
+        fi
+    elif [ "$os" == "windows" ]; then
+        rustup target add x86_64-pc-windows-gnu
+        # Compile code for Windows
+        cargo build --target="x86_64-pc-windows-gnu"
+        if [ $? -eq 0 ]; then
+            echo "Compilation for Windows successful!"
+        else
+            echo "Compilation for Windows failed!"
+            exit 12
+        fi
+    fi
+    echo ""
+done
 
 cd ..
 
 # Move produced bins to new bin directory
 
-mv navi_repo/target/x86_64-unknown-linux-gnu/debug/navi bins/navi_linux
-mv navi_repo/target/x86_64-apple-darwin/debug/navi bins/navi_macos
-mv navi_repo/target/x86_64-pc-windows-gnu/debug/navi.exe bins/navi_windows.exe
+rm -rf bins
+mkdir bins
+
+for os in "${OS[@]}"; do
+    if [ "$os" == "linux" ]; then
+        mv navi_repo/target/x86_64-unknown-linux-musl/debug/navi bins/navi_linux
+    elif [ "$os" == "macos" ]; then
+        mv navi_repo/target/x86_64-apple-darwin/debug/navi bins/navi_macos
+    elif [ "$os" == "windows" ]; then
+        mv navi_repo/target/x86_64-pc-windows-gnu/debug/navi.exe bins/navi_windows.exe
+    fi
+done
 
 # Remove navi_repo directory
 rm -rf navi_repo
